37c0966359139fa1b4cac33ea7ce496c7d0639b9
Zakaria Mekelleche <zmekell@gmail.com>
2020-07-29 14:32:45 +0100

    using classes to add product & make connection between classes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,44 +1,69 @@
-const productList = {
-  products: [
-    {
-      title: "T-Shirt",
-      imageUrl:
-        'https://dainese-cdn.thron.com/delivery/public/image/dainese/56f77d0d-bb98-445e-bf92-a04212d3948b/ramfdh/std/615x615/camo-tracks-t-shirt.jpg',
-      price: 19.88,
-      description : 'Best Choice',
-    },
-    {
-      title: 'Jean for Men',
-      imageUrl:
-        "https://www.blackdiamondequipment.com/on/demandware.static/-/Sites-bdel/default/dw9206d62c/products/S19_Product_Images/S19_Apparel/750020_4010_FORGEDDENIM_MFORGEDDENIMPANTS_Back.jpg",
-      price: 25.42,
-      description: 'Best Choice'
-    }
-  ],
+class Product {
+  title = 'Default';
+  imageUrl;
+  price;
+  description;
+
+  constructor(title, image, price, desc) {
+    this.title = title;
+    this.imageUrl = image;
+    this.price = price;
+    this.description = desc;
+  }
+}
+
+class ProductItem {
+  constructor(product) {
+    this.product = product;
+  }
 
   addProd() {
-    const htmlId = document.getElementById('app');
-    htmlId.className = 'shopping';
-    const prodList = document.createElement('div');
-    prodList.className = 'container';
-    for (const prod of this.products) {
-      const product = document.createElement('div');
-      product.className = 'card mx-auto bg-light shadow mb-4';
-      product.innerHTML = `
-        <img src="${prod.imageUrl}" alt="${prod.title}" class="card-img-top img-fluid " >
-        <div class="card-body text-center">
-          <h2>${prod.title}</h2>
-          <h5>${prod.price}</h5>
-          <p class="lead">${prod.description}</p>
-          <div class="btn btn-success">Add to Cart</div>
-        </div>
-      `;
-      prodList.append(product);
+    const product = document.createElement("div");
+    product.className = "card mx-auto bg-light shadow mb-4";
+    product.innerHTML = `
+      <img src="${this.product.imageUrl}" alt="${this.product.title}" class="card-img-top img-fluid " >
+      <div class="card-body text-center">
+        <h2>${this.product.title}</h2>
+        <h5>${this.product.price}</h5>
+        <p class="lead">${this.product.description}</p>
+        <div class="btn btn-success">Add to Cart</div>
+      </div>
+    `;
+    return product;
+  }
+}
+
+class ProductList {
+  products = [
+    new Product(
+      "T-Shirt",
+      "https://dainese-cdn.thron.com/delivery/public/image/dainese/56f77d0d-bb98-445e-bf92-a04212d3948b/ramfdh/std/615x615/camo-tracks-t-shirt.jpg",
+      19.88,
+      "Best Choice"
+    ),
+    new Product(
+      "Jean for Men",
+      "https://www.blackdiamondequipment.com/on/demandware.static/-/Sites-bdel/default/dw9206d62c/products/S19_Product_Images/S19_Apparel/750020_4010_FORGEDDENIM_MFORGEDDENIMPANTS_Back.jpg",
+      25.42,
+      "Best Choice"
+    )
+  ];
+
+  constructor() { }
   
+  addProd() {
+    const htmlId = document.getElementById("app");
+    htmlId.className = "shopping";
+    const prodList = document.createElement("div");
+    prodList.className = "container";
+    for (const prod of this.products) {
+      const productItem = new ProductItem(prod);
+      const prodId = productItem.addProd();
+      prodList.append(prodId);
     }
     htmlId.append(prodList);
   }
-};
+}
 
-
+const productList = new ProductList();
 productList.addProd();
